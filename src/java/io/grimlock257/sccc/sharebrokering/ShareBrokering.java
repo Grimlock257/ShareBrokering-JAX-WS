2301104f30cfe5081cc7067d0c83ccc7644c813c
Adam Watson <adamwatsonoa@gmail.com>
2020-11-09 21:43:52 +0000

    [S] Add missing JavaDoc and add missing @param descriptions for 'purchaseShare'

--- src/java/io/grimlock257/sccc/sharebrokering/ShareBrokering.java
+++ src/java/io/grimlock257/sccc/sharebrokering/ShareBrokering.java
@@ -34,9 +34,9 @@ public class ShareBrokering {
      * available shares by the purchase quantity amount, and attempt to marshal
      * the changed XML.
      *
-     * @param companySymbol
-     * @param quantity
-     * @return
+     * @param companySymbol The symbol of the company to purchase shares from
+     * @param quantity The amount of shares to purchase
+     * @return Whether the operation was successful
      */
     @WebMethod(operationName = "purchaseShare")
     public Boolean purchaseShare(
@@ -57,6 +57,16 @@ public class ShareBrokering {
         return false;
     }
 
+    /**
+     * Unmarshalls the Stocks XML file, then iterates over each stock, checking
+     * the symbol of that stock compared to the provided stock symbol. If this
+     * criteria is met, then increase the available shares by the sell quantity
+     * amount, and attempt to marshal the changed XML
+     *
+     * @param companySymbol The symbol of the company to sell shares to
+     * @param quantity The amount of shares to sell
+     * @return Whether the operation was successful
+     */
     @WebMethod(operationName = "sellShare")
     public Boolean sellShare(
             @WebParam(name = "companySymbol") String companySymbol,

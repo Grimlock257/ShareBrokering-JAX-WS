1a3a2aa6622fb22918af1aae8c8bdf249df93c77
Adam Watson <adamwatsonoa@gmail.com>
2020-11-11 00:04:11 +0000

    [SCCC-10] Add search web method
    Also create a StringUtil class

--- src/java/io/grimlock257/sccc/sharebrokering/ShareBrokering.java
+++ src/java/io/grimlock257/sccc/sharebrokering/ShareBrokering.java
@@ -3,6 +3,9 @@ package io.grimlock257.sccc.sharebrokering;
 import io.grimlock257.sccc.jaxb.binding.Stock;
 import io.grimlock257.sccc.jaxb.binding.Stocks;
 import io.grimlock257.sccc.sharebrokering.manager.JAXBFileManager;
+import io.grimlock257.sccc.sharebrokering.util.StringUtil;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
@@ -84,5 +87,110 @@ public class ShareBrokering {
         }
 
         return false;
+    }
+
+    /**
+     * Unmarshalls the Stocks XML file, then iterates over each stock, checking
+     * it against a set of criteria. The checks within check if the stock has
+     * <b>failed</b> against a criteria. If the end of the iteration is reached,
+     * the stock in question is added to a list to be returned to the caller.
+     * The results are finally sorted by the column and order specified.
+     *
+     * @param stockName See if value is contained in any stock names
+     * @param stockSymbol See if value is contained in any stock symbols
+     * @param currency See is value matches any currencies
+     * @param sharePriceFilter How to interpret the share price field
+     * @param sharePrice The price by which to search based on
+     * @param sortBy The column in which the results should be ordered by
+     * @param order Whether to order the sortBy column ascending or descending
+     * @return A list of filtered Stock objects based on the provided criteria
+     */
+    @WebMethod(operationName = "searchShares")
+    public List<Stock> searchShares(
+            @WebParam(name = "stockName") String stockName,
+            @WebParam(name = "stockSymbol") String stockSymbol,
+            @WebParam(name = "currency") String currency,
+            @WebParam(name = "sharePriceFilter") String sharePriceFilter,
+            @WebParam(name = "sharePrice") double sharePrice,
+            @WebParam(name = "sortBy") String sortBy,
+            @WebParam(name = "order") String order
+    ) {
+
+        List<Stock> stocks = JAXBFileManager.getInstance().unmarshal().getStocks();
+        List<Stock> filteredStocks = new ArrayList<>();
+
+        for (Stock stock : stocks) {
+            if (StringUtil.isNotNullOrEmpty(stockName) && !StringUtil.containsIgnoreCase(stock.getStockName(), stockName)) {
+                continue;
+            }
+
+            if (StringUtil.isNotNullOrEmpty(stockSymbol) && !StringUtil.containsIgnoreCase(stock.getStockSymbol(), stockSymbol)) {
+                continue;
+            }
+
+            if (StringUtil.isNotNullOrEmpty(currency) && !stock.getPrice().getCurrency().equalsIgnoreCase(currency)) {
+                continue;
+            }
+
+            if (StringUtil.isNotNullOrEmpty(sharePriceFilter) && sharePrice >= 0) {
+                switch (sharePriceFilter) {
+                    case "lessOrEqual":
+                        if (stock.getPrice().getPrice() > sharePrice) {
+                            continue;
+                        }
+
+                        break;
+                    case "equal":
+                        if (stock.getPrice().getPrice() != sharePrice) {
+                            continue;
+                        }
+
+                        break;
+                    case "greaterOrEqual":
+                        if (stock.getPrice().getPrice() < sharePrice) {
+                            continue;
+                        }
+
+                        break;
+                }
+            }
+
+            filteredStocks.add(stock);
+        }
+
+        if (StringUtil.isNotNullOrEmpty(sortBy)) {
+            switch (sortBy) {
+                case "stockName":
+                    if (StringUtil.isNotNullOrEmpty(order) && order.equalsIgnoreCase("desc")) {
+                        filteredStocks.sort(Comparator.comparing(Stock::getStockName).reversed());
+                    } else {
+                        filteredStocks.sort(Comparator.comparing(Stock::getStockName));
+                    }
+                    break;
+                case "stockSymbol":
+                    if (StringUtil.isNotNullOrEmpty(order) && order.equalsIgnoreCase("desc")) {
+                        filteredStocks.sort(Comparator.comparing(Stock::getStockSymbol).reversed());
+                    } else {
+                        filteredStocks.sort(Comparator.comparing(Stock::getStockSymbol));
+                    }
+                    break;
+                case "shareCurrency":
+                    if (StringUtil.isNotNullOrEmpty(order) && order.equalsIgnoreCase("desc")) {
+                        filteredStocks.sort(Comparator.comparing((Stock stock) -> stock.getPrice().getCurrency()).reversed());
+                    } else {
+                        filteredStocks.sort(Comparator.comparing(stock -> stock.getPrice().getCurrency()));
+                    }
+                    break;
+                case "sharePrice":
+                    if (StringUtil.isNotNullOrEmpty(order) && order.equalsIgnoreCase("desc")) {
+                        filteredStocks.sort(Comparator.comparing((Stock stock) -> stock.getPrice().getPrice()).reversed());
+                    } else {
+                        filteredStocks.sort(Comparator.comparing(stock -> stock.getPrice().getPrice()));
+                    }
+                    break;
+            }
+        }
+
+        return filteredStocks;
     }
 }
---
+++ src/java/io/grimlock257/sccc/sharebrokering/util/StringUtil.java
@@ -0,0 +1,39 @@
+package io.grimlock257.sccc.sharebrokering.util;
+
+/**
+ * StringUtil
+ *
+ * This class contains functions for dealing with Strings
+ *
+ * @author Adam Watson
+ */
+public class StringUtil {
+
+    /**
+     * Private constructor as this utility class shouldn't be instantiated
+     */
+    private StringUtil() {
+
+    }
+
+    /**
+     * Check whether the provided string is <b>not</b> null or of 0 length
+     *
+     * @param string The string to check
+     * @return True when the provided string has a value
+     */
+    public static boolean isNotNullOrEmpty(String string) {
+        return !(string == null || string.length() == 0);
+    }
+
+    /**
+     * Check whether a string contains a another string, regardless of case
+     *
+     * @param parentString The string that will be searched in
+     * @param childString The string that will be searched for
+     * @return True when the parentString contains the childString
+     */
+    public static boolean containsIgnoreCase(String parentString, String childString) {
+        return parentString.toLowerCase().contains(childString.toLowerCase());
+    }
+}

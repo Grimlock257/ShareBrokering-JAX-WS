3642d99fd25fe267d6106b48c901870d631ffce0
Adam Watson <adamwatsonoa@gmail.com>
2020-12-05 20:19:25 +0000

    [SCCC-61] Generate dummy data on startup if no existing file found

--- src/java/io/grimlock257/sccc/sharebrokering/Start.java
+++ src/java/io/grimlock257/sccc/sharebrokering/Start.java
@@ -1,6 +1,15 @@
 package io.grimlock257.sccc.sharebrokering;
 
+import io.github.grimlock257.stocks.StockPrice;
+import io.github.grimlock257.stocks.StockPriceResponse;
+import io.github.grimlock257.stocks.StockPriceSoap;
+import io.grimlock257.sccc.jaxb.binding.SharePrice;
+import io.grimlock257.sccc.jaxb.binding.Stock;
+import io.grimlock257.sccc.jaxb.binding.Stocks;
 import io.grimlock257.sccc.sharebrokering.jobs.StockPriceUpdater;
+import io.grimlock257.sccc.sharebrokering.manager.JAXBFileManager;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -17,6 +26,100 @@ public class Start {
      */
     @PostConstruct
     public void init() {
+        setupDummyData();
+
         StockPriceUpdater.initiate();
+    }
+
+    /**
+     * Set up dummy data at web service start if no shares.xml file is found
+     */
+    private void setupDummyData() {
+        if (JAXBFileManager.getInstance().unmarshal() == null) {
+            // Create new Stocks object that will be marshalled
+            Stocks stocks = new Stocks();
+            List<Stock> stocksList = stocks.getStocks();
+
+            // Create initial list of dumb stocks
+            List<InitialStock> initialStocks = new ArrayList<>();
+
+            initialStocks.add(new InitialStock("Amazon", "AMZN", 1000.99));
+            initialStocks.add(new InitialStock("Apple", "AAPL", 500.0));
+            initialStocks.add(new InitialStock("Applegreen", "APGN.IR", 50.0));
+            initialStocks.add(new InitialStock("Coca-cola", "KO", 100.5));
+            initialStocks.add(new InitialStock("National Grid", "NG.L", 25.25));
+            initialStocks.add(new InitialStock("Netflix", "NFLX", 75.01));
+            initialStocks.add(new InitialStock("Nvidia", "NVDA", 3090.0));
+            initialStocks.add(new InitialStock("Tesla", "TSLA", 5000.0));
+            initialStocks.add(new InitialStock("TomTom", "TOM2.AS", 250.0));
+
+            // Iterate over the list of initial stock data and create proper stock objects from them
+            for (InitialStock initialStock : initialStocks) {
+                StockPriceResponse stockPrice = getSharePrice(initialStock.getStockSymbol());
+
+                SharePrice sharePrice = new SharePrice();
+                sharePrice.setCurrency(stockPrice.getStockCurrency());
+                sharePrice.setPrice(stockPrice.getStockPrice());
+                sharePrice.setUpdated(stockPrice.getStockPriceTime());
+
+                Stock stock = new Stock();
+                stock.setAvailableShares(initialStock.getShareAmount());
+                stock.setStockName(initialStock.getStockName());
+                stock.setStockSymbol(initialStock.getStockSymbol());
+                stock.setPrice(sharePrice);
+
+                stocksList.add(stock);
+            }
+
+            JAXBFileManager.getInstance().marshal(stocks);
+        }
+    }
+
+    /**
+     * Private class as only used for dummy data creation
+     */
+    private class InitialStock {
+
+        private final String stockName;
+        private final String stockSymbol;
+        private final double shareAmount;
+
+        /**
+         * Represent a stock object for the initial creation
+         *
+         * @param stockName The stock name
+         * @param stockSymbol The stock symbol
+         * @param shareAmount The amount of shares
+         */
+        public InitialStock(String stockName, String stockSymbol, double shareAmount) {
+            this.stockName = stockName;
+            this.stockSymbol = stockSymbol;
+            this.shareAmount = shareAmount;
+        }
+
+        public String getStockName() {
+            return stockName;
+        }
+
+        public String getStockSymbol() {
+            return stockSymbol;
+        }
+
+        public double getShareAmount() {
+            return shareAmount;
+        }
+    }
+
+    /**
+     * Retrieve the stock price information from the remote web service for the provided stock symbol
+     *
+     * @param symbol The stock symbol for which to retrieve stock price information for
+     * @return The share price, currency and updated time contained within a StockPriceResponse object
+     */
+    private StockPriceResponse getSharePrice(String symbol) {
+        StockPrice service = new StockPrice();
+        StockPriceSoap port = service.getStockPriceSoap();
+
+        return port.getSharePrice(symbol);
     }
 }

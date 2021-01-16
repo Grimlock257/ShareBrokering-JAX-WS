c9f2a899b4571d70fa6fd09d54110a72959eadd5
Adam Watson <adamwatsonoa@gmail.com>
2021-01-16 01:08:54 +0000

    [H] Tell JAXB to serialise properly

--- src/java/io/grimlock257/sccc/sharebrokering/model/LoginResponse.java
+++ src/java/io/grimlock257/sccc/sharebrokering/model/LoginResponse.java
@@ -1,17 +1,26 @@
 package io.grimlock257.sccc.sharebrokering.model;
 
 import io.grimlock257.sccc.jaxb.binding.users.Role;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
 /**
  * Represents a login response model to return to the clients
  *
  * @author AdamW
  */
+@XmlAccessorType(XmlAccessType.FIELD)
 public class LoginResponse {
 
     private boolean successful;
     private String guid;
     private Role role;
+
+    public LoginResponse() {
+        this.successful = false;
+        this.guid = null;
+        this.role = Role.USER;
+    }
 
     private LoginResponse(boolean successful, String guid, Role role) {
         this.successful = successful;
@@ -25,5 +34,17 @@ public class LoginResponse {
 
     public static LoginResponse unsuccessfulResponse() {
         return new LoginResponse(false, null, Role.USER);
+    }
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public Role getRole() {
+        return role;
     }
 }
